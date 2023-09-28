a3258c566fce87866349e38b465674ce9303162d
Hatem Soliman <hatemthedev@gmail.com>
2023-09-28 19:20:53 +0300

    Bubble Sort - Adjusted

--- BubbleSort.java
+++ BubbleSort.java
@@ -3,9 +3,9 @@ public class BubbleSort {
     int[] a = {4, -1, 6, 5, 8, 3, 7, 2, 1, 0};
     int tmp;
     boolean bContinue = true;
-    while (bContinue == true) {
+    while (bContinue) {
       bContinue = false;
-      for (int i = 0; i < a.length - 1; i++) {
+      for (int i = 0; i <= a.length - 2; i++) {
         if (a[i + 1] < a[i]) {
           tmp = a[i];
           a[i] = a[i + 1];
